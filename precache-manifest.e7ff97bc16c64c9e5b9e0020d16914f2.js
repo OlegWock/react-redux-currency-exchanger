01660f8e17af27add43f12a1502429611363901f
OlegWock <olegwock@gmail.com>
2018-10-13 22:58:26 +0300

    Updates

---
+++ precache-manifest.e7ff97bc16c64c9e5b9e0020d16914f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5f95a4a92f7737e5bfd",
+    "url": "/currency_exchanger/static/js/runtime~main.c5f95a4a.js"
+  },
+  {
+    "revision": "cf8e96f6fac5f731e802",
+    "url": "/currency_exchanger/static/js/main.cf8e96f6.chunk.js"
+  },
+  {
+    "revision": "9c16d5cbe04af1abb2f2",
+    "url": "/currency_exchanger/static/js/1.9c16d5cb.chunk.js"
+  },
+  {
+    "revision": "cf8e96f6fac5f731e802",
+    "url": "/currency_exchanger/static/css/main.91b881d6.chunk.css"
+  },
+  {
+    "revision": "874969b2c680892cf44c0dfbf037f752",
+    "url": "/currency_exchanger/index.html"
+  }
+];
